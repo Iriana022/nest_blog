f3e8509a8e715d8bbad277b3c05a80d7629a2201
Iriana R <dev32.irin@gmail.com>
2026-01-19 17:05:35 +0300

    enable cors

--- to-training/src/main.ts
+++ to-training/src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Autorise toutes les adresses (Vercel, localhost, etc.)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
